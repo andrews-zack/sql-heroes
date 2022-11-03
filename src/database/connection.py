562433ada963e9471ad62f057d48824772a787c3
andrews-zack <zachary.andrews.au19@gmail.com>
2022-11-03 15:56:38 +0000

    lunch break

--- src/database/connection.py
+++ src/database/connection.py
@@ -32,15 +32,3 @@ def execute_query(query, params=None):
 
 # ============================================================================================================================= #
 # ============================================================================================================================= #
-
-def select_all():
-    query = """
-        SELECT * FROM heroes
-    """
-
-    list_of_heroes = execute_query(query).fetchall()
-    # print(list_of_heroes)
-    for record in list_of_heroes:
-        print(record[1])
-
-select_all()
--- src/select.py
+++ src/select.py
@@ -1,1 +1,37 @@
 from database.connection import execute_query
+from pprint import pprint as pp
+
+def select_all():
+    query = """
+        SELECT id, name FROM heroes
+    """
+
+    list_of_heroes = execute_query(query).fetchall()
+    for record in list_of_heroes:
+        pp(record)
+
+# select_all()
+
+def add_hero():
+    name = input('What is the name of the hero being added to SQL Heroes?: ')
+    about_me = input('Describe the hero in one line: ')
+    bio = input('What is the inspiring backstory of this hero?: ')
+    params = (name, about_me, bio)
+    query = """
+        INSERT INTO heroes (name, about_me, biography) VALUES(%s, %s, %s)
+        """
+    execute_query(query, params)
+
+add_hero()
+
+def remove_hero():
+    select_all()
+    whomst = input('Which hero is getting removed from SQL Heroes?: ')
+    params = (whomst,)
+    query = """
+        DELETE FROM heroes WHERE id = %s
+        """
+    execute_query(query, params)
+    select_all()
+
+remove_hero()
